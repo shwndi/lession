cca56b26365a48f7b5b6b99a1cc9f3be1f9d86ae
陈张一 <czy2132213>
2021-09-10 15:27:14 +0800

    update：补充提交

--- src/main/java/com/myself/lession/sdk/download.java
+++ src/main/java/com/myself/lession/sdk/download.java
@@ -23,13 +23,17 @@ public class download {
          * 下载位置
          */
         String port = "E://myProject//回到民国当小编";
-        extracted(url, port);
+        /**
+         * 最前面目录重复章节
+         */
+        int index = 9;
+        extracted(index,url, port);
     }
 
-    private static void extracted(String url, String port) {
+    private static void extracted(int n,String url, String port) {
         GetUrl getUrl = new GetUrl(url);
         List<Title> all = getUrl.getAll();
-        List<Title> use = all.subList(9, all.size());
+        List<Title> use = all.subList(n, all.size());
         use.stream().parallel().forEach(title -> {
             DownLoad downLoad = new DownLoad(port, title);
             downLoad.writeArticle();
